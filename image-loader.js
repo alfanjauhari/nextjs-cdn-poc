8cdd2c01e3799f2431019a81501bb9140301af1d
Alfan Jauhari <alfan@zero-one-group.com>
2025-05-09 10:10:15 +0700

    fix: image loader env

--- image-loader.js
+++ image-loader.js
@@ -1,16 +1,14 @@
-import { env } from "./src/config";
-
 export default function cloudfrontLoader({ src, width, quality }) {
 	if (
 		src.startsWith("/_next") ||
-		src.startsWith(env.NEXT_PUBLIC_BUCKET_PUBLIC_URL) ||
+		src.startsWith(process.env.NEXT_PUBLIC_BUCKET_PUBLIC_URL) ||
 		src.startsWith("http") ||
 		src.startsWith("https")
 	) {
 		return src;
 	}
 
-	const url = new URL(env.NEXT_PUBLIC_CDN_URL + src);
+	const url = new URL(process.env.NEXT_PUBLIC_CDN_URL + src);
 	url.searchParams.set("format", "auto");
 	url.searchParams.set("width", width.toString());
 	url.searchParams.set("quality", (quality || 75).toString());
